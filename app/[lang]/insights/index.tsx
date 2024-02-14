109ed228843e8d7ee19a60f3183143b504b5fce8
Tsingper <tsingper@outlook.com>
2024-02-14 21:02:08 +0800

    feat: configure env && api

--- app/[lang]/insights/index.tsx
+++ app/[lang]/insights/index.tsx
@@ -1,7 +1,27 @@
 'use client';
 import { fetcher } from '@/utils/fetcher';
+import { Card, Flex, Input } from 'antd';
 import useSWR from 'swr';
 export default function Page() {
   const { data } = useSWR('/api/user', fetcher);
-  return <h1>{data.name}</h1>;
+  return (
+    <>
+      <h1>你的专属行业洞察</h1>
+      <Flex>
+        <Card title="基本信息"></Card>
+        <Card>
+          <Input placeholder="请输入关键词" title="Prompt" />
+          <div>
+            <h2>Ai回答</h2>
+            <p>这是一个测试</p>
+          </div>
+          <div>
+            <h2>Related</h2>
+            <p>这是一个测试</p>
+          </div>
+        </Card>
+        <Card title="最新行业信息"></Card>
+      </Flex>
+    </>
+  );
 }
